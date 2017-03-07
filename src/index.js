4d6fded3207adea4677a8dac5b5c41e0a43b196c
Ximena Lasserre <ximena.lasserre@modelit.xyz>
2017-03-07 20:21:33 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainContainer from './containers/MainContainer';
+import './index.css';
+
+ReactDOM.render(
+  <MainContainer />,
+  document.getElementById('root')
+);
